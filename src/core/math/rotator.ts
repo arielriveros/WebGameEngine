32cc207b2a3e3744cde032d0e8f340869a1b7d1b
Ariel <ariel.riveros@ing.uchile.cl>
2022-05-17 19:26:29 -0400

    fix: Rotator's addition value clamping

--- src/core/math/rotator.ts
+++ src/core/math/rotator.ts
@@ -3,9 +3,6 @@ export class Rotator {
     private _yaw: number;
     private _pitch: number;
 
-    private PI: number = 3.141592653589793;
-    private PI_2: number = 1.5707963267948966;
-
     public constructor(yaw: number = 0, pitch: number = 0, roll: number = 0) {
         this._roll = roll % 360;
         this._yaw = yaw % 360;
@@ -24,9 +21,9 @@ export class Rotator {
     public set pitch(value: number) { this._pitch = value % 360; }
 
     public add(rotator: Rotator): void {
-        this._roll += rotator.roll % 360;
-        this._yaw += rotator.yaw % 360;
-        this._pitch += rotator.pitch % 360;
+        this._roll = (this._roll + rotator.roll) % 360;
+        this._yaw = (this._yaw + rotator.yaw) % 360;
+        this._pitch = (this._pitch + rotator.pitch) % 360;
     }
 
 }
