2207b3457bb498532ff6fd1b2d9e514bd4742cd0
Ariel <ariel.riveros@ing.uchile.cl>
2022-05-17 14:14:13 -0400

    refactor: Logger clean up

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,37 +1,40 @@
-export function LOG(input: string, type: string = 'normal', onBrowser: boolean = false) {
+export function LOG(input: string, type: string = 'normal', onBrowser: boolean = false, overwrite: boolean = false): void {
+
+    let debugConsole = document.getElementById('console') as HTMLDivElement;
+
     let entry: HTMLDivElement = document.createElement('div');
     entry.className = 'console-entry';
 
     let text: HTMLDivElement = document.createElement('div');
     switch(type){
         case 'error': 
-            text.className = 'console-error'; 
+            text.className = 'console-text console-error'; 
             if(onBrowser){
                 console.error(input);
             }
             break;
         case 'info':
-            text.className = 'console-info';
+            text.className = 'console-text console-info';
             if(onBrowser){
                 console.info(input);
             }
             break;
         case 'warning':
-            text.className = 'console-warning';
+            text.className = 'console-text console-warning';
             if(onBrowser){
                 console.warn(input);
             }
             break;
         case 'normal':
         default:
-            text.className = 'console-text';
+            text.className = 'console-text console-normal';
             if(onBrowser){
                 console.log(input);
             }
             break;
 
     }
-    
+
     text.innerHTML = input;
 
     const date = new Date();
@@ -42,7 +45,6 @@ export function LOG(input: string, type: string = 'normal', onBrowser: boolean =
     entry.appendChild(text);
     entry.appendChild(time);
 
-    let debugConsole = document.getElementById('console');
     debugConsole?.append(entry);
     debugConsole?.scrollTo(0, debugConsole.scrollHeight);
 }
