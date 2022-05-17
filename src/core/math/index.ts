d24783ed387a371e87aae881ec9ec5efdd471c2f
Ariel <ariel.riveros@ing.uchile.cl>
2022-05-17 19:27:11 -0400

    refactor: Pi constants defined in module

--- src/core/math/index.ts
+++ src/core/math/index.ts
@@ -2,4 +2,7 @@ import { Vector2, Vector3, Vector4 } from "./vector";
 import { Matrix4x4 } from "./matrix";
 import { Rotator } from "./rotator";
 
-export { Vector2, Vector3, Vector4, Matrix4x4, Rotator };
+const PI: number = 3.141592653589793;
+const PI_2: number = 1.5707963267948966;
+
+export { Vector2, Vector3, Vector4, Matrix4x4, Rotator, PI, PI_2 };
