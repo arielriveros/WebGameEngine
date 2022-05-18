f8116a6ec68b3868e8c0e19e70f177f7f12d10d1
Ariel <ariel.riveros@ing.uchile.cl>
2022-05-18 03:16:52 -0400

    feat: Added numeric keys

--- src/core/input/inputMaps.ts
+++ src/core/input/inputMaps.ts
@@ -11,7 +11,18 @@ export const KeyboardMapping: {[name: string]: {[name: string]: number}} = {
     'ArrowUp':      {'keyCode': 38},
     'ArrowRight':   {'keyCode': 39},
     'ArrowDown':    {'keyCode': 40},
-    
+
+    'Digit0':   {'keyCode': 48},
+    'Digit1':   {'keyCode': 49},
+    'Digit2':   {'keyCode': 50},
+    'Digit3':   {'keyCode': 51},
+    'Digit4':   {'keyCode': 52},
+    'Digit5':   {'keyCode': 53},
+    'Digit6':   {'keyCode': 54},
+    'Digit7':   {'keyCode': 55},
+    'Digit8':   {'keyCode': 56},
+    'Digit9':   {'keyCode': 57},
+
     'KeyA': {'keyCode': 65},
     'KeyB': {'keyCode': 66},
     'KeyC': {'keyCode': 67},
@@ -37,5 +48,16 @@ export const KeyboardMapping: {[name: string]: {[name: string]: number}} = {
     'KeyW': {'keyCode': 87},
     'KeyX': {'keyCode': 88},
     'KeyY': {'keyCode': 89},
-    'KeyZ': {'keyCode': 90}
+    'KeyZ': {'keyCode': 90},
+
+    'Numpad0':  {'keyCode': 96},
+    'Numpad1':  {'keyCode': 97},
+    'Numpad2':  {'keyCode': 98},
+    'Numpad3':  {'keyCode': 99},
+    'Numpad4':  {'keyCode': 100},
+    'Numpad5':  {'keyCode': 101},
+    'Numpad6':  {'keyCode': 102},
+    'Numpad7':  {'keyCode': 103},
+    'Numpad8':  {'keyCode': 104},
+    'Numpad9':  {'keyCode': 105}
 }
