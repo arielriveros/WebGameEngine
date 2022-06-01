b36c5d005465291c678bc05cc519f1bfeb9156e5
Ariel <ariel.riveros@ing.uchile.cl>
2022-06-01 16:53:08 -0400

    feat: Color tint in textured shaders

--- src/core/rendering/shaders/sources/textured.fs.glsl
+++ src/core/rendering/shaders/sources/textured.fs.glsl
@@ -1,9 +1,10 @@
 precision mediump float;
 
+varying vec3 v_color;
 varying vec2 v_texCoord;
 
 uniform sampler2D u_texture;
 
 void main() {
-    gl_FragColor = texture2D(u_texture, v_texCoord);
+    gl_FragColor = texture2D(u_texture, v_texCoord) * vec4(v_color, 1.0);
 }
--- src/core/rendering/shaders/sources/textured.vs.glsl
+++ src/core/rendering/shaders/sources/textured.vs.glsl
@@ -1,13 +1,16 @@
 precision mediump float;
 
 attribute vec3 a_position;
+attribute vec3 a_color;
 attribute vec2 a_texCoord;
 
+varying vec3 v_color;
 varying vec2 v_texCoord;
 
 uniform mat4 u_viewProj;
         
 void main() {
+    v_color = a_color;
     v_texCoord = a_texCoord;
     gl_Position = u_viewProj * vec4(a_position, 1.0);
 }
