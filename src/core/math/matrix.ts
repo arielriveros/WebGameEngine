46c6853453c928ffa93f36ee2c456efcdaacb61d
Ariel <ariel.riveros@ing.uchile.cl>
2022-06-02 03:59:58 -0400

    feat: Inverse, transpose matrix operations

--- src/core/math/matrix.ts
+++ src/core/math/matrix.ts
@@ -2,6 +2,7 @@ import { Rotator } from './rotator';
 import { Vector2, Vector3, Vector4 } from './vector'
 
 export class Matrix4x4 {
+    
     private _data: number[] = [];
     public constructor() {
         this._data = [
@@ -453,6 +454,103 @@ export class Matrix4x4 {
         out._data[7] = a13 * c - a03 * s;
         return out;
     }
+
+    public static invert(out: Matrix4x4, a: Matrix4x4) {
+      let a00 = a._data[0],
+        a01 = a._data[1],
+        a02 = a._data[2],
+        a03 = a._data[3];
+      let a10 = a._data[4],
+        a11 = a._data[5],
+        a12 = a._data[6],
+        a13 = a._data[7];
+      let a20 = a._data[8],
+        a21 = a._data[9],
+        a22 = a._data[10],
+        a23 = a._data[11];
+      let a30 = a._data[12],
+        a31 = a._data[13],
+        a32 = a._data[14],
+        a33 = a._data[15];
+      let b00 = a00 * a11 - a01 * a10;
+      let b01 = a00 * a12 - a02 * a10;
+      let b02 = a00 * a13 - a03 * a10;
+      let b03 = a01 * a12 - a02 * a11;
+      let b04 = a01 * a13 - a03 * a11;
+      let b05 = a02 * a13 - a03 * a12;
+      let b06 = a20 * a31 - a21 * a30;
+      let b07 = a20 * a32 - a22 * a30;
+      let b08 = a20 * a33 - a23 * a30;
+      let b09 = a21 * a32 - a22 * a31;
+      let b10 = a21 * a33 - a23 * a31;
+      let b11 = a22 * a33 - a23 * a32;
+      // Calculate the determinant
+      let det =
+        b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+      if (!det) {
+        return null;
+      }
+      det = 1.0 / det;
+      out._data[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+      out._data[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+      out._data[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+      out._data[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+      out._data[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+      out._data[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+      out._data[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+      out._data[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+      out._data[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+      out._data[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+      out._data[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+      out._data[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+      out._data[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+      out._data[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+      out._data[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+      out._data[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+      return out;
+    }
+
+    public static transpose(out: Matrix4x4, a: Matrix4x4) {
+      if (out === a) {
+        let a01 = a._data[1],
+          a02 = a._data[2],
+          a03 = a._data[3];
+        let a12 = a._data[6],
+          a13 = a._data[7];
+        let a23 = a._data[11];
+        out._data[1] = a._data[4];
+        out._data[2] = a._data[8];
+        out._data[3] = a._data[12];
+        out._data[4] = a01;
+        out._data[6] = a._data[9];
+        out._data[7] = a._data[13];
+        out._data[8] = a02;
+        out._data[9] = a12;
+        out._data[11] = a._data[14];
+        out._data[12] = a03;
+        out._data[13] = a13;
+        out._data[14] = a23;
+      } else {
+        out._data[0] = a._data[0];
+        out._data[1] = a._data[4];
+        out._data[2] = a._data[8];
+        out._data[3] = a._data[12];
+        out._data[4] = a._data[1];
+        out._data[5] = a._data[5];
+        out._data[6] = a._data[9];
+        out._data[7] = a._data[13];
+        out._data[8] = a._data[2];
+        out._data[9] = a._data[6];
+        out._data[10] = a._data[10];
+        out._data[11] = a._data[14];
+        out._data[12] = a._data[3];
+        out._data[13] = a._data[7];
+        out._data[14] = a._data[11];
+        out._data[15] = a._data[15];
+      }
+      return out;
+    }
+
 
     public static rotateWithRotator(out: Matrix4x4, a: Matrix4x4, rotator: Rotator): Matrix4x4 {
         Matrix4x4.rotateY(out, out, rotator.getRadiansYaw());
