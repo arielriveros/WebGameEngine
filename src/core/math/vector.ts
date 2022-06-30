466436ad2dce330477fb6d8edaf08ec323625f90
Ariel <ariel.riveros@ing.uchile.cl>
2022-06-30 02:28:50 -0400

    refactor: Vectors implemented as object instead of array
    Weird memory issues when cloning vectors as arrays, switched to object implementation for better memory management and cloning

--- src/core/math/vector.ts
+++ src/core/math/vector.ts
@@ -1,42 +1,42 @@
 export class Vector
 {
-    protected _data: number[];
+    private _x: number;
+    private _y: number;
 
     public constructor(x: number = 0, y: number = 0)
     {
-        this._data = [x, y];
+        this._x = x;
+        this._y = y;
     }
 
     public get x(): number
     {
-        return this._data[0];
+        return this._x;
     }
 
     public set x(value: number)
     {
-        this._data[0] = value;
+        this._x = value;
     }
 
     public get y(): number
     {
-        return this._data[1];
+        return this._y;
     }
 
     public set y(value: number)
     {
-        this._data[1] = value;
+        this._y = value;
     }
 
     public length(): number
     {
-        let sum: number = 0.0;
-        this._data.forEach( i => { sum += i * i; }  );
-        return Math.sqrt(sum);
+        return Math.sqrt(this._x * this._x + this._y * this._y);
     }
 
     public toArray(): number[]
     {
-        return this._data;
+        return [this._x, this._y];
     }
 
     public toFloat32Array(): Float32Array
@@ -46,86 +46,251 @@ export class Vector
 
     public toString(): string
     {
-        const map = ['x', ', y', ', z', ', w']
-        let out = '{';
-        for (const [i, e] of this._data.entries())
-        {
-            out += `${map[i]}: ${e}`;
-        }
-        out += '}';
-        return out;
+        return `{x: ${this._x}, y: ${this._y}}`;
+    }
+
+    public clone(): Vector
+    {
+        return new Vector(this._x, this._y);
     }
 
     public add(v: Vector): Vector
     {
-        for (const i in this._data)
-        {
-            this._data[i] += v._data[i];
-        }
-
+        this._x += v._x;
+        this._y += v._y;
         return this;
     }
 
-    public multiply(v: number): Vector
+    public scale(v: number): Vector
     {
-        for (const i in this._data)
-        {
-            this._data[i] * v;
-        }
+        this._x *= v;
+        this._y *= v;
+        return this;
+    }
 
+    public multiply(v: Vector): Vector
+    {
+        this._x *= v._x;
+        this._y *= v._y;
         return this;
     }
 
     public opposite(): Vector
     {
-        for (const i in this._data)
-        {
-            this._data[i] = -this._data[i];
-        }
-
+        this.scale(-1);
         return this;
     }
 }
 
-export class Vector3 extends Vector
+export class Vector3
 {
-    
+    private _x: number;
+    private _y: number;
+    private _z: number;
+
     public constructor(x: number = 0, y: number = 0, z: number = 0)
     {
-        super(x, y);
-        this._data.push(z);
+        this._x = x;
+        this._y = y;
+        this._z = z;
+    }
+
+    public get x(): number
+    {
+        return this._x;
+    }
+
+    public set x(value: number)
+    {
+        this._x = value;
+    }
+
+    public get y(): number
+    {
+        return this._y;
+    }
+
+    public set y(value: number)
+    {
+        this._y = value;
     }
 
     public get z(): number
     {
-        return this._data[2];
+        return this._z;
     }
 
     public set z(value: number)
     {
-        this._data[2] = value;
+        this._z = value;
     }
 
-    public clone(): Vector3 {
-        return new Vector3(this.x, this.y, this.z);
+    public length(): number
+    {
+        return Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z);
+    }
+
+    public toArray(): number[]
+    {
+        return [this._x, this._y, this._z];
+    }
+
+    public toFloat32Array(): Float32Array
+    {
+        return new Float32Array(this.toArray());
+    }
+
+    public toString(): string
+    {
+        return `{x: ${this._x}, y: ${this._y}, z: ${this._z}}`;
+    }
+
+    public clone(): Vector3
+    {
+        return new Vector3(this._x, this._y, this._z);
+    }
+
+    public add(v: Vector3): Vector3
+    {
+        this._x += v._x;
+        this._y += v._y;
+        this._z += v._z;
+        return this;
+    }
+
+    public scale(v: number): Vector3
+    {
+        this._x *= v;
+        this._y *= v;
+        this._z *= v;
+        return this;
+    }
+
+    public multiply(v: Vector3): Vector3
+    {
+        this._x *= v._x;
+        this._y *= v._y;
+        this._z *= v._z;
+        return this;
+    }
+
+    public opposite(): Vector3
+    {
+        this.scale(-1);
+        return this;
     }
 }
 
-export class Vector4 extends Vector3
+export class Vector4
 {
+    private _x: number;
+    private _y: number;
+    private _z: number;
+    private _w: number;
+
     public constructor(x: number = 0, y: number = 0, z: number = 0, w: number = 0)
     {
-        super(x, y, z);
-        this._data.push(w);
+        this._x = x;
+        this._y = y;
+        this._z = z;
+        this._w = w;
+    }
+
+    public get x(): number
+    {
+        return this._x;
+    }
+
+    public set x(value: number)
+    {
+        this._x = value;
+    }
+
+    public get y(): number
+    {
+        return this._y;
+    }
+
+    public set y(value: number)
+    {
+        this._y = value;
+    }
+
+    public get z(): number
+    {
+        return this._z;
+    }
+
+    public set z(value: number)
+    {
+        this._z = value;
     }
 
     public get w(): number
     {
-        return this._data[3];
+        return this._w;
     }
 
     public set w(value: number)
     {
-        this._data[3] = value;
+        this._w = value;
+    }
+
+    public length(): number
+    {
+        return Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z + this._w * this._w);
+    }
+
+    public toArray(): number[]
+    {
+        return [this._x, this._y, this._z, this._w];
+    }
+
+    public toFloat32Array(): Float32Array
+    {
+        return new Float32Array(this.toArray());
+    }
+
+    public toString(): string
+    {
+        return `{x: ${this._x}, y: ${this._y}, z: ${this._z}, w: ${this._w}}`;
+    }
+
+    public clone(): Vector4
+    {
+        return new Vector4(this._x, this._y, this._z, this._w);
+    }
+
+    public add(v: Vector4): Vector4
+    {
+        this._x += v._x;
+        this._y += v._y;
+        this._z += v._z;
+        this._w += v._w;
+        return this;
+    }
+
+    public scale(v: number): Vector4
+    {
+        this._x *= v;
+        this._y *= v;
+        this._z *= v;
+        this._w *= v;
+        return this;
+    }
+
+    public multiply(v: Vector4): Vector4
+    {
+        this._x *= v._x;
+        this._y *= v._y;
+        this._z *= v._z;
+        this._w *= v._w;
+        return this;
+    }
+
+    public opposite(): Vector4
+    {
+        this.scale(-1);
+        return this;
     }
 }
