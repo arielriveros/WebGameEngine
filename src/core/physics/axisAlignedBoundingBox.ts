66ee65a0e7c337e91edd90393860494dff2429ed
Ariel <ariel.riveros@ing.uchile.cl>
2022-07-19 03:09:14 -0400

    refactor: Cleaning code

--- src/core/physics/axisAlignedBoundingBox.ts
+++ src/core/physics/axisAlignedBoundingBox.ts
@@ -32,14 +32,6 @@ export class AxisAlignedBouningBox extends CollisionShape
         return point.x >= this.min.x && point.x <= this.max.x &&
                point.y >= this.min.y && point.y <= this.max.y &&
                point.z >= this.min.z && point.z <= this.max.z;
-
-        /* let x: number = point.x;
-        let y: number = point.y;
-        let z: number = point.z;
-
-        return x >= this.position.x - this._width / 2 && x <= this.position.x + this._width / 2 &&
-               y >= this.position.y - this._height / 2 && y <= this.position.y + this._height / 2 &&
-               z >= this.position.z - this._depth / 2 && z <= this.position.z + this._depth / 2; */
     }
 
     public override collides(other: CollisionShape): boolean
@@ -49,12 +41,9 @@ export class AxisAlignedBouningBox extends CollisionShape
             let otherMin: Vector3 = other.min;
             let otherMax: Vector3 = other.max;
 
-            let min: Vector3 = this.min;
-            let max: Vector3 = this.max;
-
-            return otherMin.x <= max.x && otherMax.x >= min.x &&
-                   otherMin.y <= max.y && otherMax.y >= min.y &&
-                   otherMin.z <= max.z && otherMax.z >= min.z;
+            return this.min.x <= otherMax.x && otherMin.x <= this.max.x && 
+                   this.min.y <= otherMax.y && otherMin.y <= this.max.y && 
+                   this.min.z <= otherMax.z && otherMin.z <= this.max.z  
         }
         return false;
     }
